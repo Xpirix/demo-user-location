4155d159f1fc7ff35f99cfef85279079d62b3aa8
Lova ANDRIARIMALALA <43842786+Xpirix@users.noreply.github.com>
2023-09-14 15:09:37 +0300

    Setup the frontend using argon template and boostrap

--- appUsers/views.py
+++ appUsers/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-# Create your views here.
+@login_required
+def profile(request):
+    user = request.user
+    return render(request, 'dashboard/profile.html', {'user': user})
